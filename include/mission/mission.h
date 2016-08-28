a48f9a6eeac5f99d649c5ea24f4f4a3da1b04e8c
huanglilong <huanglilongwk@outlook.com>
2016-08-28 20:08:37 +0800

    sample state machine for main state, tested with gazebo

--- include/mission/mission.h
+++ include/mission/mission.h
@@ -11,10 +11,31 @@
 #include <math.h>
 #include <geometry_msgs/PoseStamped.h>
 #include <geometry_msgs/TwistStamped.h>
-#include <mavros_msgs/State.h>
-#include <mavros_msgs/CommandTOL.h>
+#include <mission/State.h>
+#include <mission/CommandTOL.h>
 
 namespace mission{
+
+/*
+ * @brief  main state
+ *
+ */
+enum MAIN_STATE
+{
+	M_IDLE =0,
+	M_DISPLAY,
+	M_DIGITAL_0,
+	M_DIGITAL_1,
+	M_DIGITAL_2,
+	M_DIGITAL_3,
+	M_DIGITAL_4,
+	M_DIGITAL_5,
+	M_DIGITAL_6,
+	M_DIGITAL_7,
+	M_DIGITAL_8,
+	M_DIGITAL_9,
+	M_LAND
+};
 
 /*
  * @brief  state machine class
@@ -24,19 +45,42 @@ namespace mission{
  {
 	 public:
 	 	Mission();
-		~Mission(){};
-	 
+		~Mission();
+
+		void mission_main(void);							// entry point
+
+		geometry_msgs::PoseStamped _current_local_pos;		// uav current local postion
+		mission::State 			   _current_state;			// uav current arm status and mode
+
+		geometry_msgs::PoseStamped _local_pos_sp; 			// actual local pos sent to uav
+
+		uint8_t _main_state;								// main state for state machine
+		ros::NodeHandle _node_handle;
+		ros::Rate _rate;
 	 private:
+		 // state machine
+		 void state_machine(void);
+
+		 // callback functions
+		 void state_cb(const mission::State::ConstPtr& msg);
+		 void local_pos_cb(const geometry_msgs::PoseStamped::ConstPtr& msg);
+
+		 void wait_connect(void);
+
+		 void cmd_streams(void);				// pixhawk need cmd streams, before switch to OFFBOARD mode
+
 	 	 // Subscribers 
 		 ros::Subscriber _state_sub;			// get pixhawk's arming and status
 		 ros::Subscriber _cur_pos_sub;			// get pixhawk current local position
 
 		 // Publishers
-		 ros::Publisher _local_pos_pub;			// pusblish local position setpoint to pixhawk
+		 ros::Publisher _local_pos_sp_pub;		// pusblish local position setpoint to pixhawk
 
 		 // Services
 		 ros::ServiceClient _land_client;		// land command
- }
 
+		 mission::CommandTOL _landing_cmd;
+		 ros::Time _landing_last_request;
+	};
 
 }	// end of mission namespace
--- src/mission/mission.cpp
+++ src/mission/mission.cpp
@@ -4,3 +4,152 @@
 * @author   : huang li long <huanglilongwk@outlook.com>
 * @time     : 2016/08/26
 */
+
+#include "mission.h"
+
+using namespace mission;
+
+// constructor
+Mission::Mission():
+_main_state(M_IDLE),
+_rate(20.0)
+{
+    // clear all members's value here
+
+}
+
+// deconstructor
+Mission::~Mission()
+{
+
+}
+
+// public interface
+void
+Mission::mission_main(void)
+{
+    // subscribers init
+    _state_sub = _node_handle.subscribe("mavros/state", 10, &Mission::state_cb, this);
+    _cur_pos_sub = _node_handle.subscribe("mavros/local_position/pose", 10, &Mission::local_pos_cb, this);
+
+    // publishers init
+    _local_pos_sp_pub = _node_handle.advertise<geometry_msgs::PoseStamped>("mavros/setpoint_position/local", 10);
+
+    // services init
+    _land_client = _node_handle.serviceClient<mission::CommandTOL>("mavros/cmd/land");
+
+    // wait for FCU connection
+    wait_connect();
+
+    //send a few setpoints before switch to OFFBOARD mode
+    cmd_streams();
+
+    // for landing service
+    _landing_last_request = ros::Time::now();
+
+    // state machine
+    while(ros::ok()){
+
+        // for debug convenience
+		if(!_current_state.armed)
+		{
+			_main_state = M_IDLE;
+		}
+
+        state_machine();
+        ros::spinOnce();
+        _rate.sleep();
+    }
+}
+
+void
+Mission::state_machine(void)
+{
+    /***************** for test ***********************/
+    geometry_msgs::PoseStamped pose_a;
+    geometry_msgs::PoseStamped pose_b;
+    pose_a.pose.position.x = 0;
+    pose_a.pose.position.y = 0;
+    pose_a.pose.position.z = 5;
+    pose_b.pose.position.x = 0;
+    pose_b.pose.position.y = 5;
+    pose_b.pose.position.z = 5;
+    /***************** for test ***********************/
+
+    switch(_main_state){
+        case M_IDLE:
+            _local_pos_sp_pub.publish(_current_local_pos);
+            if(_current_state.armed && _current_state.mode == "OFFBOARD")    // start mission
+            {
+                _main_state = M_DISPLAY;
+            }
+            break;
+        case M_DISPLAY:
+            _local_pos_sp_pub.publish(pose_a);      // publish display's local position
+            if((abs(_current_local_pos.pose.position.x - pose_a.pose.position.x) < 0.1) &&
+               (abs(_current_local_pos.pose.position.y - pose_a.pose.position.y) < 0.1) &&
+               (abs(_current_local_pos.pose.position.z - pose_a.pose.position.z) < 0.1))
+               {
+                    _main_state = M_DIGITAL_0; // get digital number from display
+               }
+            break;
+        case M_DIGITAL_0:
+            _local_pos_sp_pub.publish(pose_b);      // start sub state machine, here just publish local pos for debug convenience
+            if((abs(_current_local_pos.pose.position.x - pose_b.pose.position.x) < 0.1) &&
+               (abs(_current_local_pos.pose.position.y - pose_b.pose.position.y) < 0.1) &&
+               (abs(_current_local_pos.pose.position.z - pose_b.pose.position.z) < 0.1))
+               {
+                    _main_state = M_LAND;
+               }
+            break;
+        case M_LAND:
+            if(_current_state.mode == "OFFBOARD"){
+
+                if(_current_state.mode != "AUTO.LAND" &&
+                (ros::Time::now() - _landing_last_request > ros::Duration(5.0))){
+                if(_land_client.call(_landing_cmd) &&
+                    _landing_cmd.response.success){
+                    ROS_INFO("AUTO LANDING!");
+                }
+                _landing_last_request = ros::Time::now();
+                }
+            }
+            break;
+    }
+}
+
+// uav state subscriber's callback function
+void
+Mission::state_cb(const mission::State::ConstPtr& msg)
+{
+    _current_state = *msg;
+}
+
+// local pos subscriber's callback function
+void
+Mission::local_pos_cb(const geometry_msgs::PoseStamped::ConstPtr& msg)
+{
+    _current_local_pos = *msg;
+}
+
+// wait for mavros connecting with pixhawk
+void
+Mission::wait_connect(void)
+{
+    while(ros::ok() && !_current_state.connected){
+        ros::spinOnce();
+        _rate.sleep();
+    }
+}
+
+// send commands streams to pixhawk before switch to OFFBOARD mode
+void
+Mission::cmd_streams(void)
+{
+    // publish current local position
+    for(int i = 100; ros::ok() && i > 0; --i){
+        _local_pos_sp_pub.publish(_current_local_pos);
+        ros::spinOnce();
+        _rate.sleep();
+    }
+}
