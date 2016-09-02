74517c09f9bfea9a75c301c90d23660f904a42f9
huanglilong <huanglilongwk@outlook.com>
2016-09-02 11:12:57 +0800

    subsrcibe attitude msg from pixhawk for vision using

---
+++ src/vision_att_node.cpp
@@ -0,0 +1,32 @@
+/**
+* @file  	vision_att_node.cpp
+* @brief 	example: subscribe pixhawk attitude msg for vision using
+* @author	huang li long <huanglilongwk@outlook.com>
+*/
+
+#include <ros/ros.h>
+#include <mission/Attitude.h>
+
+mission::Attitude att_msg;
+void att_cb(const mission::Attitude::ConstPtr& msg){
+    att_msg = *msg;
+	// TODO with att_msg
+}
+
+int main(int argc, char **argv)
+{
+    ros::init(argc, argv, "vision_att_node");
+    ros::NodeHandle nh;
+
+    ros::Subscriber att_sub = nh.subscribe<mission::Attitude>("mavros/attitude", 10, att_cb);
+
+    ros::Rate rate(20.0);
+
+    while(ros::ok()){
+	
+        ros::spinOnce();
+        rate.sleep();
+    }
+
+    return 0;
+}
