8cd8b3348082fe9892cc5019130fd0ee83c4bd91
bing0037 <bike_travel@163.com>
2017-09-30 10:00:39 +0800

    last commit from hll

--- src/vision_att_node.cpp
+++ src/vision_att_node.cpp
@@ -6,11 +6,22 @@
 
 #include <ros/ros.h>
 #include <mission/Attitude.h>
+#include <geometry_msgs/TransformStamped.h>
+ros::Publisher attitude_publisher;
 
 mission::Attitude att_msg;
-void att_cb(const mission::Attitude::ConstPtr& msg){
+void att_cb(const mission::Attitude::ConstPtr& msg)
+{
     att_msg = *msg;
-	ROS_INFO("Attitude roll: %f", att_msg.roll);
+    geometry_msgs::TransformStamped imu_att;
+    imu_att.header.frame_id = "attitude";
+    imu_att.header.stamp    = ros::Time::now();
+    imu_att.transform.rotation.x = att_msg.roll;
+    imu_att.transform.rotation.y = att_msg.pitch;
+    imu_att.transform.rotation.z = att_msg.yaw;
+    attitude_publisher.publish(imu_att);
+    //printf("roll=%f; pit=%f; yaw=%f;\n", att_msg.roll, att_msg.pitch, att_msg.yaw);
+    //ROS_INFO("yaw=%f;", att_msg.yaw);
 	// TODO with att_msg
 }
 
@@ -19,11 +30,13 @@ int main(int argc, char **argv)
     ros::init(argc, argv, "vision_att_node");
     ros::NodeHandle nh;
 
-    ros::Subscriber att_sub = nh.subscribe<mission::Attitude>("mavros/attitude", 10, att_cb);
+    ros::Subscriber att_sub = nh.subscribe<mission::Attitude>("mavros/attitude", 1, att_cb);
+    attitude_publisher = nh.advertise<geometry_msgs::TransformStamped>("imu/attitude",1);
 
-    ros::Rate rate(20.0);
+    ros::Rate rate(30.0);
 
-    while(ros::ok()){
+    while(ros::ok())
+    {
 	
         ros::spinOnce();
         rate.sleep();
